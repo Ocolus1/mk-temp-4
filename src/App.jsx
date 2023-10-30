0301e4c40b181f9f3207d6d7bcb310bbd0c3f916
Paul <awolesiboluwatife@gmail.com>
2023-10-30 18:46:21 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import HeroSection from "./Hero/HeroSection";
+import GrowthSection from "./Growth/GrowthSection";
+import BenefitSection from "./Benefits/BenefitSection";
+import PricingSection from "./Pricing/PricingSection";
+import TestimonialSection from "./Testimonial/TestimonialSection";
+import Footer from "./Footer/Footer";
+
+function App() {
+  return (
+    <div className="idk__4">
+      <HeroSection />
+      <GrowthSection />
+      <BenefitSection />
+      <TestimonialSection />
+      <PricingSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
